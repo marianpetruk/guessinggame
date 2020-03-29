33f481a70009e2a0c8d36aac2eb77d408cbb6245
Marian Petruk <marian.petruk@gmail.com>
2020-03-29 17:45:36 +0300

    add function for checking answer

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+function check_answer {
+    if [[ $1 -eq $2 ]]
+    then
+      echo "Congrats! You guessed correctly!"
+    elif [[ $1 -gt $2 ]]
+    then
+      echo "Your guess is too high."
+    else
+      echo "Your guess is too low."
+    fi
+}
+
+number_of_files_in_wd=$(ls | wc -l)
+
+#number_of_all_files_in_wd=$(ls -l | wc -l)
+#echo $number_of_files_in_wd
+
 echo "Type your guess on how many files are in the current directory and then press Enter:"
 read response
 echo "You entered: $response"
+
+check_answer $response $number_of_files_in_wd
+
+#if [[ $response -eq $number_of_files_in_wd ]]
+
+
+
+
