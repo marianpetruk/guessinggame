20ee864231cc30c3496032508f38ef924ac69b0c
Marian Petruk <marian.petruk@gmail.com>
2020-03-29 17:32:09 +0300

    read user guess

---
+++ guessinggame.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+echo "Type your guess on how many files are in the current directory and then press Enter:"
+read response
+echo "You entered: $response"
