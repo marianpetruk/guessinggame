6ef697c9798b9bce03d05c4dced382ebe4e817b6
Marian Petruk <marian.petruk@gmail.com>
2020-03-29 17:53:25 +0300

    add loop

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-function check_answer {
-    if [[ $1 -eq $2 ]]
-    then
-      echo "Congrats! You guessed correctly!"
-    elif [[ $1 -gt $2 ]]
-    then
-      echo "Your guess is too high."
-    else
-      echo "Your guess is too low."
-    fi
-}
-
+return_code=1
 number_of_files_in_wd=$(ls | wc -l)
-
 #number_of_all_files_in_wd=$(ls -l | wc -l)
 #echo $number_of_files_in_wd
 
-echo "Type your guess on how many files are in the current directory and then press Enter:"
-read response
-echo "You entered: $response"
+function check_answer {
+    if [[ $1 -eq $2 ]]
+    then
+      echo "Congrats! You guessed correctly!";
+      return 0;
+    elif [[ $1 -gt $2 ]]
+    then
+      echo -e "Your guess is too high.\n"
+    else
+      echo -e "Your guess is too low.\n"
+    fi
+    return 1;
+}
 
-check_answer $response $number_of_files_in_wd
-
-#if [[ $response -eq $number_of_files_in_wd ]]
-
-
-
-
+while [ $return_code -eq 1 ]
+do
+    echo "Type your guess on how many files are in the current directory and then press Enter:";
+    read response;
+    echo "You entered: $response";
+    check_answer $response $number_of_files_in_wd
+    return_code=$?;
+done
